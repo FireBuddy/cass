0023ec220c67d92a401c26889628ab0226eb4838
FireBuddy <aijun@live.ca>
2016-06-14 17:57:36 -0400

    Update Cassiopeia.cs

--- KoreanAIO/KoreanAIO/Champions/Cassiopeia.cs
+++ KoreanAIO/KoreanAIO/Champions/Cassiopeia.cs
@@ -76,7 +76,7 @@ namespace KoreanAIO.Champions
             
             Obj_AI_Base.OnBasicAttack += delegate (Obj_AI_Base Sender, GameObjectProcessSpellCastEventArgs args)
             {
-                if (Sender.Type == GameObjectType.AIHeroClient && Sender.IsValidTarget(Q.Range) && Q.IsReady())
+                if (sender.IsMe)
                 {
                  
                 }
