b978d679445d631abf914228e794335de6de10a3
FireBuddy <aijun@live.ca>
2016-06-14 17:59:20 -0400

    Update Cassiopeia.cs

--- KoreanAIO/KoreanAIO/Champions/Cassiopeia.cs
+++ KoreanAIO/KoreanAIO/Champions/Cassiopeia.cs
@@ -76,7 +76,7 @@ namespace KoreanAIO.Champions
             
             Obj_AI_Base.OnBasicAttack += delegate (Obj_AI_Base sender, GameObjectProcessSpellCastEventArgs args)
             {
-                if (sender.IsMe)
+                if (sender.Type == GameObjectType.AIHeroClient )
                 {
                   Q.Cast(sender.ServerPosition + 20);
                 }
