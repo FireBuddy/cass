891054462399c3cf63d390d2b9549ceb97ca8c8d
FireBuddy <aijun@live.ca>
2016-06-20 14:49:33 -0400

    Update Syndra.cs

--- KoreanAIO/KoreanAIO/Champions/Syndra.cs
+++ KoreanAIO/KoreanAIO/Champions/Syndra.cs
@@ -205,7 +205,7 @@ namespace KoreanAIO.Champions
             };
             Gapcloser.OnGapcloser += delegate (AIHeroClient sender, Gapcloser.GapcloserEventArgs args)
             {
-                if (sender.IsEnemy && AutomaticMenu.CheckBox("Gapcloser") && args.End.Distance(MyHero, true) <= sender.Distance(MyHero, true) && args.EndPos.Distance(MyHero, true) <= E.Range)
+                if (sender.IsEnemy && AutomaticMenu.CheckBox("Gapcloser") && args.End.Distance(MyHero, true) <= sender.Distance(MyHero, true) && args.End.Distance(MyHero, true) <= 600)
                 {
                     E.Cast(args.End);
                     CastQE(sender);
