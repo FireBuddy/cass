542d7b9ea6f12aae765d8c9fae19ee9eb4c643da
FireBuddy <aijun@live.ca>
2016-06-20 11:15:52 -0400

    Update Syndra.cs

--- KoreanAIO/KoreanAIO/Champions/Syndra.cs
+++ KoreanAIO/KoreanAIO/Champions/Syndra.cs
@@ -178,7 +178,7 @@ namespace KoreanAIO.Champions
                     }
                 }
                 {
-                if (!sender.IsDashing() && sender == Target && (ModeManager.LastHit || ModeManager.Flee || ModeManager.Harass) && sender.IsValidTarget(1300) && Q.IsReady  && sender.IsEnemy && sender.Type == GameObjectType.AIHeroClient)
+                if (!sender.IsDashing() && sender == Target && (ModeManager) && sender.IsValidTarget(1300) && Q.IsReady  && sender.IsEnemy && sender.Type == GameObjectType.AIHeroClient)
                 {    
                     if (Q.IsReady && sender.IsValidTarget(825))
                     {
