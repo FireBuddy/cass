e44ed9d96971449dccef905e1e9eaefc1c348301
FireBuddy <aijun@live.ca>
2016-06-28 21:30:46 -0400

    Update Zed.cs

--- KoreanAIO/KoreanAIO/Champions/Zed.cs
+++ KoreanAIO/KoreanAIO/Champions/Zed.cs
@@ -69,7 +69,7 @@ namespace KoreanAIO.Champions
                  {
                      W.Cast(sender.ServerPosition);
                  }
-                 if(Q.IsReady && W.isoncooldown)
+                 if(Q.IsReady && W.Isoncooldown)
                  {
                      Q.Cast(sender.ServerPosition);
                  }
